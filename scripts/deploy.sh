cbe9867bbbe559bbb1a540c9e661e4157e66897d
Cullen Taylor <mctaylor@us.ibm.com>
2019-02-08 14:23:54 -0600

    Add deploy stage to .gitlab-ci.yml
    Signed-off-by: Cullen Taylor <mctaylor@us.ibm.com>

---
+++ scripts/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ibmcloud plugin install -f kubernetes-service
+ibmcloud plugin install -f container-registry
+
+ibmcloud login                     \
+  --apikey "${IBMCLOUD_API_KEY}"   \
+  -a "https://api.ng.bluemix.net"  \
+  -r "us-south"
+
+ibmcloud target           \
+  -o "Developer Advocacy" \
+  -s "dev"
+
+ibmcloud cr login
+ibmcloud cr region-set us-south
+
+while [ -z "${KUBECONFIG}" ]; do
+  $(ibmcloud ks cluster-config --export eggshell-gl-12)
+done
+
+kubectl version
+kubectl cluster-info
+
+ibmcloud cr build --tag registry.ng.bluemix.net/eggshell/status_page:1 .
+kubectl apply -f deploy/status-namespace.yaml
+
+kubectl get secret bluemix-default-secret --export -o yaml | kubectl apply --namespace=status-page -f -
+kubectl get secret bluemix-default-secret-regional --export -o yaml | kubectl apply --namespace=status-page -f -
+kubectl get secret bluemix-default-secret-international --export -o yaml | kubectl apply --namespace=status-page -f -
+
+kubectl apply -f deploy/redis-deployment.yaml
+kubectl apply -f deploy/status-deployment.yaml
